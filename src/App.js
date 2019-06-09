cb63b390ef289e583adf3d222f1605eb1964efac
MinSun Park <minsun@MinSuns-MacBook-Pro.local>
2019-06-09 12:48:50 -0400

    install & connect to material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Icon>star</Icon>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
